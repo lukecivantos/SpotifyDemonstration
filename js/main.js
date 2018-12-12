e3504f0b42c95f66bcb55698be5262e44ac5b7fb
Luke Civantos <lfcivantos@gmail.com>
2018-12-12 16:01:59 -0500

    push reader

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@
 
 var data;
 var fiveRandomMembers;
-
+var current = null;
 d3.csv("data/playlistExample.csv", function(d){
     data = d
 });
 
 $(document).ready(function() {
-    $('body').css('visibility','visible').hide().fadeIn(2000);
+    $('body').css('visibility','visible').hide().fadeIn(1500);
     $('#chart-area').css('visibility','visible').hide();
 });
 
@@ -36,11 +36,12 @@ function getPlaylists() {
         $('#' + i.toString()).text(fiveRandomMembers[i].name)
     }
 
-    $('#chart-area').css('visibility','visible').hide().fadeIn(2000);
+    $('#chart-area').css('visibility','visible').hide().fadeIn(1500);
 }
 
 
 function getPlaylist(i) {
+
     var firstVariable = fiveRandomMembers[i].playlist.split("'), ");
     firstVariable = firstVariable.map(function (d) {
         var x = d.split(', ');
@@ -75,9 +76,18 @@ function getPlaylist(i) {
         return x
     });
 
+    if (current == null) {
+        $('#refreshSection').css('visibility','visible').hide().fadeIn(1500);
+        $('#scrollArea').css('visibility','visible').hide().fadeIn(1500);
+        $('#scrollArea2').css('visibility','visible').hide().fadeIn(1500);
+    }
+
+    current = secondVariable;
+    var newVariable = secondVariable.slice(0,5);
+
     console.log(secondVariable);
     // JavaScript
-    var recommended = secondVariable.map(function (d) {
+    var recommended = newVariable.map(function (d) {
         return '<tr><strong>' + d[0] + " by " +  d[1] + '</strong></tr>'
     });
 
@@ -97,6 +107,43 @@ function getPlaylist(i) {
 }
 
 
+function refreshPlaylist() {
+    var currentPlaylist = current;
+
+    function getRandomSubarray(arr, size) {
+        var shuffled = arr.slice(0), i = arr.length, temp, index;
+        while (i--) {
+            index = Math.floor((i + 1) * Math.random());
+            temp = shuffled[index];
+            shuffled[index] = shuffled[i];
+            shuffled[i] = temp;
+        }
+        return shuffled.slice(0, size);
+    }
+
+    console.log(currentPlaylist);
+
+    var newVariable = getRandomSubarray(currentPlaylist, 5);
+
+
+    var recommended = newVariable.map(function (d) {
+        return '<tr><strong>' + d[0] + " by " +  d[1] + '</strong></tr>'
+    });
+
+
+    var clusterize2 = new Clusterize({
+        rows: recommended,
+        scrollId: 'scrollArea2',
+        contentId: 'contentArea2'
+    });
+    //for (i = 0; i < 5; i++)
+    //{
+    //    $('#' + i.toString()).text(fiveRandomMembers[i].name)
+    //}
+
+    //$('#chart-area').css('visibility','visible').hide().fadeIn(2000);
+
+}
 
 
 
